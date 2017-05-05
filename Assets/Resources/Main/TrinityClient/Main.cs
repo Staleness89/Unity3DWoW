0c7b0b9f4f9329af51302e8f39e1308dadf36595
caffeine239 <caffeine239@yahoo.com>
2017-05-05 02:38:00 -0400

    Update Main.cs

--- Assets/Resources/Main/TrinityClient/Main.cs
+++ Assets/Resources/Main/TrinityClient/Main.cs
@@ -10,9 +10,9 @@ public class Main : MonoBehaviour {
     void Start()
     {
 
-        GameObject myBrick = Instantiate(login, new Vector3(Screen.width / 2, Screen.height / 2, 0), Quaternion.identity);
-        myBrick.transform.parent = transform;
-        myBrick.name = "Login";
+        GameObject mainLogin = Instantiate(login, new Vector3(Screen.width / 2, Screen.height / 2, 0), Quaternion.identity);
+        mainLogin.transform.parent = transform;
+        mainLogin.name = "Login";
     }
 
     // Update is called once per frame
